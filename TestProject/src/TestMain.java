f65fb9900c8c10747b2d4ea371d7c154abecdfbc
Abdelilah <abdelilahchaib19@augustana.edu>
2022-09-28 18:44:04 -0500

    "Added dancing and saying goodbye."

--- TestProject/src/TestMain.java
+++ TestProject/src/TestMain.java
@@ -1,6 +1,7 @@
 
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.print("“Hello Chicken World!");
+		System.out.print("Hello Chicken World!");
+		System.out.print("Goodbye");
 	}
 }
